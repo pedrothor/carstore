9c7259d6d91b295fd8a19b5f41dff1ab0cdf1e33
pedrothor <pedro.thor@hotmail.com>
2024-03-18 22:50:34 -0300

    choices de civil_status retirado e birth alterado para Charfield

--- employees/models.py
+++ employees/models.py
@@ -1,23 +1,13 @@
 from django.db import models
 from units.models import Unit
 
-CIVIL_STATUS = [
-    ('single', 'Single'),
-    ('married', 'Married'),
-    ('divorced', 'Divorced'),
-    ('widowed', 'Widowed'),
-    ('engaged', 'Engaged'),
-    ('common-law marriage', 'Common-law marriage'),
-]
-
-
 class Employee(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    birth = models.DateField()
+    birth = models.CharField(max_length=10)
     unit = models.ForeignKey(Unit, related_name='unit_employee', on_delete=models.PROTECT)
     address = models.CharField(max_length=200)
-    civil_status = models.CharField(max_length=200, choices=CIVIL_STATUS)
+    civil_status = models.CharField(max_length=200)
     image = models.ImageField(upload_to='employees')
 
     def __str__(self):
